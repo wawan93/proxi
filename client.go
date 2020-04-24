07190c04febb194774d4e5c304a7d5b541584a42
wawan93 <wawan1331@gmail.com>
2020-04-24 23:51:49 +0300

    add proxy pool

--- client.go
+++ client.go
@@ -10,19 +10,9 @@ type ProxyPool interface {
 }
 
 func Client(pool ProxyPool) (*http.Client, error) {
-	// rand.Seed(time.Now().UnixNano())
-	// proxies, err := pool.Update()
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// if len(proxies) == 0 {
-	// 	return nil, fmt.Errorf("proxy pool is empty")
-	// }
-
 	c := &http.Client{
 		Transport: &http.Transport{
 			Proxy: func(r *http.Request) (*url.URL, error) {
-				// proxy := proxies[rand.Intn(len(proxies))]
 				proxy := pool.Random()
 				u, err := url.Parse(proxy)
 				if err != nil {
---
+++ pool/sslproxies.go
@@ -0,0 +1,88 @@
+/*
+ProxyPool implementation for sslproxies.org proxy provider
+*/
+package pool
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+)
+
+type SSLProxiesConfig struct {
+	Email       string
+	Pass        string
+	Pid         string
+	ShowCountry bool
+	Https       bool
+}
+
+type SSLProxiesPool struct {
+	proxies []string
+	m       sync.Mutex
+	cfg     *SSLProxiesConfig
+}
+
+func NewSSLProxiesPool(cfg *SSLProxiesConfig) (*SSLProxiesPool, error) {
+	p := &SSLProxiesPool{
+		cfg: cfg,
+	}
+	return p, p.Update()
+}
+
+func (p *SSLProxiesPool) Update() error {
+	query := &url.Values{}
+	query.Add("email", p.cfg.Email)
+	query.Add("pass", p.cfg.Pass)
+	query.Add("pid", p.cfg.Pid)
+	showcountry := "no"
+	if p.cfg.ShowCountry {
+		showcountry = "yes"
+	}
+	query.Add("showcountry", showcountry)
+	https := "no"
+	if p.cfg.Https {
+		https = "yes"
+	}
+	query.Add("https", https)
+
+	resp, err := http.Get("http://list.didsoft.com/get?" + query.Encode())
+	if err != nil {
+		return fmt.Errorf("cannot get proxies: %v", err)
+	}
+	defer resp.Body.Close()
+
+	proxiesBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("cannot parse proxies: %v", err)
+	}
+	proxies := strings.Split(string(proxiesBytes), "\n")
+
+	result := make([]string, 0)
+	for _, proxy := range proxies {
+		result = append(result, "http://"+strings.TrimSpace(proxy))
+	}
+	log.Printf("fetched %d proxies", len(result))
+
+	if len(result) == 0 {
+		return fmt.Errorf("proxy pool is empty")
+	}
+
+	p.m.Lock()
+	p.proxies = result
+	p.m.Unlock()
+	return nil
+}
+
+func (p *SSLProxiesPool) Random() string {
+	rand.Seed(time.Now().UnixNano())
+	p.m.Lock()
+	defer p.m.Unlock()
+	return p.proxies[rand.Intn(len(p.proxies))]
+}
