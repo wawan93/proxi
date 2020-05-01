8f39d508ec395ae2e14160193c42f0c4df231c6e
wawan93 <wawan1331@gmail.com>
2020-05-01 13:29:08 +0300

    add Fetch func

--- client_test.go
+++ client_test.go
@@ -1,27 +1,31 @@
 package proxi_test
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 
 	"github.com/wawan93/proxi"
 )
 
-var cnt int
-var proxiServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	cnt++
-}))
+type p struct {
+	url string
+}
 
-type pool struct{}
-
-func (p *pool) Random() string {
-	return proxiServer.URL
+func (p *p) Random() string {
+	return p.url
 }
 
 func TestGetClient(t *testing.T) {
-	p := &pool{}
+	var cnt int
+	var proxiServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cnt++
+	}))
+
+	p := &p{url: proxiServer.URL}
 	c, err := proxi.Client(p)
 	if err != nil {
 		t.Error(err)
@@ -35,5 +39,37 @@ func TestGetClient(t *testing.T) {
 
 	if cnt == 0 {
 		t.Error("proxi was not used")
+	}
+}
+
+func TestFetch(t *testing.T) {
+	var cnt int
+	var m sync.Mutex
+	var proxiServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m.Lock()
+		cnt++
+		m.Unlock()
+	}))
+
+	p := &p{url: proxiServer.URL}
+	c, err := proxi.Client(p)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+	url := ts.URL
+
+	c, err = proxi.Client(p)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = proxi.Fetch(context.Background(), c, url, nil)
+	if err != nil {
+		t.Error(err)
 	}
 }
